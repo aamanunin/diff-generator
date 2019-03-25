aa6c33ac0d4a0a17699ebaa69f46737e3497cedb
Aleksandr Maniunin <aamanunin@gmail.com>
2019-03-25 12:28:55 +0300

    init project

---
+++ src/bin/gendiff.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
+import program from 'commander';
+import { version } from '../../package.json';
+
+program
+  .version(version)
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .parse(process.argv);
