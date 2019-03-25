27a078ecd973d479f46e75be78995cff75f70891
Aleksandr Maniunin <aamanunin@gmail.com>
2019-03-25 18:52:41 +0300

    rework structure

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,10 +6,19 @@
 
 import program from 'commander';
 import { version } from '../../package.json';
+import getDiffOf2JSONFiles from '../lib/diffJSON';
 
 program
   .version(version)
-  .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'Output format')
-  .parse(process.argv);
+  .option('-f, --format [type]', 'Output format');
+
+program
+  .arguments('<firstConfig> <secondConfig>')
+  .action(getDiffOf2JSONFiles);
+
+program.parse(process.argv);
+
+const getDiffOf2Files = (pathToFile1, pathToFile2) => program.action(pathToFile1, pathToFile2);
+
+export default getDiffOf2Files;
---
+++ src/lib/diffJSON.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+const getDiffOf2JSONFiles = (pathToFile1, pathToFile2) => {
+  console.log(pathToFile1);
+  console.log(pathToFile2);
+};
+
+export default getDiffOf2JSONFiles;
