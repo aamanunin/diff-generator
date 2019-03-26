54d39cf7c1ed066c3b105a36d4fbef57c80148ab
Aleksandr Maniunin <aamanunin@gmail.com>
2019-03-27 01:57:09 +0300

    fix test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,9 +5,6 @@ import genDiff from '../src';
 test('genDiff', () => {
   const relativePath1 = '__tests__/__fixtures__/testBefore.json';
   const relativePath2 = '__tests__/__fixtures__/testAfter.json';
-  const absolutePath1 = '/home/aleksandr/WorkFolder/gendiff/__tests__/__fixtures__/testBefore.json';
-  const absolutePath2 = '/home/aleksandr/WorkFolder/gendiff/__tests__/__fixtures__/testAfter.json';
   const pathToResult = '__tests__/__fixtures__/result';
   expect(genDiff(relativePath1, relativePath2)).toBe(fs.readFileSync(path.resolve(path.normalize(pathToResult)), 'utf8'));
-  expect(genDiff(absolutePath1, absolutePath2)).toBe(fs.readFileSync(path.resolve(path.normalize(pathToResult)), 'utf8'));
 });
