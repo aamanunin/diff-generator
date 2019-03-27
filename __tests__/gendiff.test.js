0de7a8f157de8f3ff442c0ce5f688b8919a4b149
Aleksandr Maniunin <aamanunin@gmail.com>
2019-03-27 22:04:41 +0300

    refactoring project

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,5 +6,7 @@ test('genDiff', () => {
   const relativePath1 = '__tests__/__fixtures__/testBefore.json';
   const relativePath2 = '__tests__/__fixtures__/testAfter.json';
   const pathToResult = '__tests__/__fixtures__/result';
-  expect(genDiff(relativePath1, relativePath2)).toBe(fs.readFileSync(path.resolve(path.normalize(pathToResult)), 'utf8'));
+  const actual = genDiff(relativePath1, relativePath2);
+  const expected = fs.readFileSync(path.resolve(path.normalize(pathToResult)), 'utf8');
+  expect(actual).toBe(expected);
 });
--- src/index.js
+++ src/index.js
@@ -4,22 +4,53 @@ import _ from 'lodash';
 
 const getContent = pathFile => fs.readFileSync(path.resolve(path.normalize(pathFile)), 'utf8');
 
+const propertyActions = [
+  {
+    name: 'not changed',
+    check: (data1, data2, key) => (_.has(data1, key) && _.has(data2, key)
+      && (data1[key] === data2[key])),
+    process: (data1, data2, key) => [`    ${key}: ${data1[key]}`],
+  },
+  {
+    name: 'changed',
+    check: (data1, data2, key) => (_.has(data1, key) && _.has(data2, key)
+      && (data1[key] !== data2[key])),
+    process: (data1, data2, key) => [`  + ${key}: ${data2[key]}`, `  - ${key}: ${data1[key]}`],
+  },
+  {
+    name: 'deleted',
+    check: (data1, data2, key) => (_.has(data1, key) && !_.has(data2, key)),
+    process: (data1, data2, key) => [`  - ${key}: ${data1[key]}`],
+  },
+  {
+    name: 'added',
+    check: (data1, data2, key) => (!_.has(data1, key) && _.has(data2, key)),
+    process: (data1, data2, key) => [`  + ${key}: ${data2[key]}`],
+  },
+];
+
+const getPropertyAction = (data1, data2, key) => propertyActions
+  .find(({ check }) => check(data1, data2, key));
+
+const getDifferences = (data1 = {}, data2 = {}) => {
+  const dataKeys = _.union(Object.keys(data1), Object.keys(data2));
+  return dataKeys.reduce((acc, key) => {
+    const { process } = getPropertyAction(data1, data2, key);
+    return [...acc, ...process(data1, data2, key)];
+  }, []);
+};
+
+const render = elements => `{\n${elements.join('\n')}\n}`;
+
 const genDiff = (pathToFile1, pathToFile2) => {
   const contentFile1 = getContent(pathToFile1);
   const contentFile2 = getContent(pathToFile2);
   const dataFile1 = JSON.parse(contentFile1);
   const dataFile2 = JSON.parse(contentFile2);
-  const getAST = (data1 = {}, data2 = {}) => {
-    const dataKeys = _.union(Object.keys(data1), Object.keys(data2));
-    return `{\n${dataKeys.reduce((acc, key) => {
-      if (_.has(data1, key) && _.has(data2, key) && (data1[key] === data2[key])) return `${acc}    ${key}: ${data1[key]}\n`;
-      if (_.has(data1, key) && _.has(data2, key) && (data1[key] !== data2[key])) return `${acc}  + ${key}: ${data2[key]}\n  - ${key}: ${data1[key]}\n`;
-      if (_.has(data1, key) && !_.has(data2, key)) return `${acc}  - ${key}: ${data1[key]}\n`;
-      if (!_.has(data1, key) && _.has(data2, key)) return `${acc}  + ${key}: ${data2[key]}\n`;
-      return acc;
-    }, '')}}`;
-  };
-  return getAST(dataFile1, dataFile2);
+
+  const differences = getDifferences(dataFile1, dataFile2);
+
+  return render(differences);
 };
 
 export default genDiff;
