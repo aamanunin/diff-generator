c2973c2326ed219325b1239284731fe6f040b58e
Aleksandr Maniunin <aamanunin@gmail.com>
2019-03-30 00:54:51 +0300

    added logic for working with multi-level data

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,11 +9,15 @@ describe('genDiff', () => {
   const pathTOYml2 = '__tests__/__fixtures__/testAfter.yml';
   const pathToIni1 = '__tests__/__fixtures__/testBefore.ini';
   const pathToIni2 = '__tests__/__fixtures__/testAfter.ini';
+  const pathToJSONTree1 = '__tests__/__fixtures__/testBeforeTree.json';
+  const pathToJSONTree2 = '__tests__/__fixtures__/testAfterTree.json';
   const pathToResult = '__tests__/__fixtures__/result';
+  const pathToResultTree = '__tests__/__fixtures__/resultTree';
   test.each([
     [pathToJSON1, pathToJSON2, pathToResult],
     [pathToYml1, pathTOYml2, pathToResult],
     [pathToIni1, pathToIni2, pathToResult],
+    [pathToJSONTree1, pathToJSONTree2, pathToResultTree],
   ])(
     '%s and %s',
     (path1, path2, path3) => {
--- src/index.js
+++ src/index.js
@@ -14,26 +14,31 @@ const getData = (pathToFile) => {
 
 const propertyActions = [
   {
-    name: 'not changed',
+    type: 'parent',
+    check: (data1, data2, key) => (data1[key] instanceof Object && data2[key] instanceof Object),
+    process: (value1, value2, func) => func(value1, value2),
+  },
+  {
+    type: 'notChanged',
     check: (data1, data2, key) => (_.has(data1, key) && _.has(data2, key)
       && (data1[key] === data2[key])),
-    process: (data1, data2, key) => [`    ${key}: ${data1[key]}`],
+    process: value1 => value1,
   },
   {
-    name: 'changed',
+    type: 'changed',
     check: (data1, data2, key) => (_.has(data1, key) && _.has(data2, key)
       && (data1[key] !== data2[key])),
-    process: (data1, data2, key) => [`  + ${key}: ${data2[key]}`, `  - ${key}: ${data1[key]}`],
+    process: (value1, value2) => ({ before: value1, after: value2 }),
   },
   {
-    name: 'deleted',
+    type: 'deleted',
     check: (data1, data2, key) => (_.has(data1, key) && !_.has(data2, key)),
-    process: (data1, data2, key) => [`  - ${key}: ${data1[key]}`],
+    process: value1 => value1,
   },
   {
-    name: 'added',
+    type: 'added',
     check: (data1, data2, key) => (!_.has(data1, key) && _.has(data2, key)),
-    process: (data1, data2, key) => [`  + ${key}: ${data2[key]}`],
+    process: (value1, value2) => value2,
   },
 ];
 
@@ -42,15 +47,38 @@ const getPropertyAction = (data1, data2, key) => propertyActions
 
 const buildAst = (data1 = {}, data2 = {}) => {
   const dataKeys = _.union(Object.keys(data1), Object.keys(data2));
-  return dataKeys.reduce((acc, key) => {
-    const { process } = getPropertyAction(data1, data2, key);
-    const value = process(data1, data2, key);
-    return [...acc, value];
-  }, []);
+
+  return dataKeys.map((key) => {
+    const { type, process } = getPropertyAction(data1, data2, key);
+    const value = process(data1[key], data2[key], buildAst);
+    const valueOrChildren = () => ((type === 'parent') ? 'children' : 'value');
+
+    return { key, type, [valueOrChildren()]: value };
+  });
 };
 
-const render = elements => `{\n${_.flatten(elements).join('\n')}\n}`;
+const stringify = (data, depth) => {
+  if (!(data instanceof Object)) return data;
+  const keys = Object.keys(data);
+  return `{\n${keys.map(key => `${' '.repeat(depth * 4 + 4)}${key}: ${data[key]}\n`)}${' '.repeat(depth * 4)}}`;
+};
 
+
+const typeActions = {
+  parent: (difference, depth, func) => `${' '.repeat(depth * 4)}${difference.key}: {\n${func(difference.children, depth + 1)}\n${' '.repeat(depth * 4)}}`,
+  notChanged: (difference, depth) => `${' '.repeat(depth * 4)}${difference.key}: ${stringify(difference.value, depth)}`,
+  changed: (difference, depth) => `${' '.repeat(depth * 4 - 2)}- ${difference.key}: ${stringify(difference.value.before, depth)}\n${' '.repeat(depth * 4 - 2)}+ ${difference.key}: ${stringify(difference.value.after, depth)}`,
+  deleted: (difference, depth) => `${' '.repeat(depth * 4 - 2)}- ${difference.key}: ${stringify(difference.value, depth)}`,
+  added: (difference, depth) => `${' '.repeat(depth * 4 - 2)}+ ${difference.key}: ${stringify(difference.value, depth)}`,
+};
+
+const action = (difference, depth, func) => typeActions[difference.type](difference, depth, func);
+
+const render = (collection) => {
+  const renderAst = (ast, depth = 1) => _.flatten(ast.map(difference => `${action(difference, depth, renderAst)}`)).join('\n');
+
+  return `{\n${renderAst(collection)}\n}`;
+};
 export default (pathToFile1, pathToFile2) => {
   const data1 = getData(pathToFile1);
   const data2 = getData(pathToFile2);
